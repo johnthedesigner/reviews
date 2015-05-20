af1fdcb66f8a2f5ac6edeac774fe70ac3cb1546a
johnthedesigner <john@johnthedesigner.com>
2015-05-20 15:55:40 -0400

    Link user in table to single user view

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -25,7 +25,8 @@
 						<?php
 							foreach ( $users as $user ) {
 								echo '<tr>';
-								echo action('UserController@show', $user['name'], array('id' => $user['id']));							
+								$user_link = action('UserController@show', array('id' => $user['id']));
+								echo '<td><a href="'.$user_link.'">Edit</a></td>';	
 								foreach ( $user as $value ) {
 									echo '<td>'.$value.'</td>';
 								}
