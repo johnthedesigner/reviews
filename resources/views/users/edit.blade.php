779164131cdce63ccfd68c97b8fb00a7ba7f4181
johnthedesigner <john@johnthedesigner.com>
2015-05-20 20:07:51 -0400

    added edit view and controller function

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -25,7 +25,7 @@
 						<div class="form-group">
 							<label class="col-md-4 control-label">Name</label>
 							<div class="col-md-6">
-								<input type="text" class="form-control" name="name" value="{{ $user->name) }}">
+								<input type="text" class="form-control" name="name" value="{{ $user->name }}">
 							</div>
 						</div>
 
