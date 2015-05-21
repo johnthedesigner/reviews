e30a564b47d496d4de9a24439f816ffbf88a8b60
johnthedesigner <john@johnthedesigner.com>
2015-05-20 22:47:08 -0400

    added edit view and controller function

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -19,7 +19,7 @@
 						</div>
 					@endif
 
-					<form class="form-horizontal" role="form" method="PATCH" action="{{ url('/users/'.$user['id']) }}">
+					<form class="form-horizontal" role="form" method="PATCH" action="{{ url('/users/'.$user['id'].'/update') }}">
 						<input type="hidden" name="_token" value="{{ csrf_token() }}">
 
 						<div class="form-group">
