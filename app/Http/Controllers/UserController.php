a930a12bab4ee4f7ce6b738e793dfe09f4d14935
johnthedesigner <john@johnthedesigner.com>
2015-05-22 22:59:24 -0400

    Added role field to user edit view

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,6 +9,25 @@ use View;
 
 class UserController extends Controller {
 
+	// ROLES AND PERMISSIONS
+	// Admin Role
+	$admin = new Role();
+	$admin->name         = 'admin';
+	$admin->display_name = 'Administrator'; // optional
+	$admin->description  = 'User is allowed to manage and edit other users'; // optional
+	$admin->save();
+
+	// Admin Area Permission
+	$adminView = new Permission();
+	$adminView->name         = 'admin-view';
+	$adminView->display_name = 'Admin View'; // optional
+	// Allow a user to...
+	$adminView->description  = 'User can access Admin area'; // optional
+	$adminView->save();
+	
+	$admin->attachPermission($adminView);
+	// equivalent to $admin->perms()->sync(array($adminView->id));
+	
 	/*
 	* Require authenticated user
 	*/
