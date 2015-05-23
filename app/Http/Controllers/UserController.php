599298235e6a08eaf0a9be80e1dca4cf0b002169
johnthedesigner <john@johnthedesigner.com>
2015-05-22 23:25:45 -0400

    Added role field to user edit view

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -74,7 +74,7 @@ class UserController extends Controller {
 	public function edit($id)
 	{
 		$user = User::find($id)->toArray();
-		$roles = Auth::user()->ability(array('admin', 'owner'));
+		$roles = User::find($id)->ability(array('admin', 'owner'));
 
 		return View::make('users.edit',array('user'=>$user,'roles'=>$roles));
 	}
